e0c3fb1d9f02d00df754fdd41d209f30eb611735
Subin Choi <mybin1020@gamail.com>
2022-03-15 10:35:56 +0900

    0315 sb 수정3

---
+++ src/app/pages/main-gate/frame/auction/index.js
@@ -0,0 +1,165 @@
+import {React, useEffect, useRef} from "react";
+import Style from './style.module.css'
+import { useHistory } from 'react-router-dom'
+
+// swiper bundle styles
+import 'swiper/swiper-bundle.min.css'
+import 'swiper/swiper.min.css'
+import 'swiper/components/navigation/navigation.min.css'
+import 'swiper/components/pagination/pagination.min.css'
+
+
+import {Swiper, SwiperSlide} from "swiper/react";
+import SwiperCore, {
+  Mousewheel,Pagination,Navigation
+} from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([Mousewheel,Pagination,Navigation]);
+
+
+
+
+const Auction = () => {
+    const history = useHistory()
+
+    return (
+        <>
+            <div 
+            className={Style["pointer"]}
+            onClick={
+                () => {
+                    history.push('/auction')
+                }
+            }
+            >
+                맵분양이동
+            </div>
+            <Swiper 
+                direction={"vertical"}
+                slidesPerView={1}
+                spaceBetween={30}
+                mousewheel={true}
+                resizeObserver={true}
+                pagination={{clickable: true}}
+                className={Style['mySwiper']}
+            >
+                <SwiperSlide className={Style['swiper-slide']}>
+                    <h2 className={Style["auction-title"]}>Auction 이용 방법</h2>
+                    <div className={Style["auctionBox"]}>
+                        <div className={Style['img1']}></div>
+                        <div className={Style['auction-content-text']}>
+                            <h3>step 1</h3>
+                            <p>‘이루고 월드’ 홈페이지 로그인 </p>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className={Style['swiper-slide']}>
+                    <h2 className={Style["auction-title"]}>Auction 이용 방법</h2>
+                    <div className={Style["auctionBox"]}>
+                        <div className={Style['img2']}></div>
+                        <div className={Style['auction-content-text']}>
+                            <div>
+                                <h3>step 2</h3>
+                                <p>맵 분양 페이지 접속</p>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className={Style['swiper-slide']}>
+                    <h2 className={Style["auction-title"]}>Auction 이용 방법</h2>
+                    <div className={Style["auctionBox"]}>
+                        <div className={Style['img3']}></div>
+                        <div className={Style['auction-content-text']}>
+                            <div>
+                                <h3>step 3</h3>
+                                <p>구역 및 위치, 땅 수량 선택</p>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className={Style['swiper-slide']}>
+                    <h2 className={Style["auction-title"]}>Auction 이용 방법</h2>
+                    <div className={`${Style.auctionBox} ${Style.auctionImgbox}`}>
+                        <div className={Style['auction-img']}>
+                            <div className={Style['img5']}></div>
+                            <div className={Style['auction-img-text']}>구매가능</div>
+                        </div>
+                        <div className={Style['auction-img']}>
+                            <div className={Style['img6']}></div>
+                            <div className={Style['auction-img-text']}>선택한 범위내 특정 셀 확인</div>
+                        </div>
+                        <div className={Style['auction-img']}>
+                            <div className={Style['img7']}></div>
+                            <div className={Style['auction-img-text']}>기존 입찰자 존재</div>
+                        </div>
+                        <div className={Style['auction-img']}>
+                            <div className={Style['img8']}></div>
+                            <div className={Style['auction-img-text']}>기존 입찰 범위 선택</div>
+                        </div>
+                        <div className={Style['auction-img']}>
+                            <div className={Style['img9']}></div>
+                            <div className={Style['auction-img-text']}>구매 불가 지역</div>
+                        </div>
+                        <div className={Style['map-content']}>※ 맵 선택 : 최소 1개 ~ 최대 4,096개</div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className={Style['swiper-slide']}>
+                    <h2 className={Style["auction-title"]}>Auction 이용 방법</h2>
+                    <div className={Style['auctionBox']}>
+                        <div className={Style['img4']}></div>
+                        <div className={Style['auction-content-text']}>
+                            <div>
+                                <h3>step 4</h3>
+                                <p>우측 상단 입찰가 기입 후 경매 참가</p>
+                            </div>
+                            <div className={Style['auction-content-text-font']}>
+                                <p>PositionX : 선택 땅의 X축 좌표</p>
+                                <p>PositionY : 선택 땅의 Y축 좌표</p>
+                                <p>Max Bid : 현재 최고 입찰가</p>
+                                <p>Bid Price : 입찰 희망가</p>
+                                <p>Bid Price 입찰 희망가 입력 후 하단의 Apply 클릭하여 경매 참여</p>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className={Style['swiper-slide']}>
+                <h2 className={Style["auction-title"]}>Auction 이용 방법</h2>
+                    <div className={Style["auctionBox"]}>
+                        <div className={Style['step5-img']}></div>
+                        <div className={Style['auction-content-text']}>
+                            <div>
+                                <h3>step 5</h3>
+                                <p>Auction 관리자 메시지 확인</p>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className={Style['swiper-slide']}>
+                    <h2 className={Style["auction-title"]}>Auction 이용 방법</h2>
+                    <div className={Style["auctionBox"]}>
+                        <div className={Style['auction-howto-content']}>
+                            <div>
+                                <p>※ 경매 진행 기간</p>
+                                <p>‘이루고 월드’의 땅 분양 경매는 22.xx.xx(00시) ~ 22.xx.xx(00시)까지 2주간 진행됩니다.</p>
+                            </div>
+                            <div>
+                                <h4>※ 주의 사항</h4>
+                                <ul>
+                                    <li>① 맵 분양은 경매 형식으로 진행되며, 낙찰 받지 못한 EWC는 계정 지갑으로 환급됩니다.</li>
+                                    <li>② 경매 입찰 취소는 My Bid에서 가능합니다.</li>
+                                    <li>③ 검은색 픽셀은 구매가 불가합니다.</li>
+                                    <li>④ 개인 하우징은 최소 10개의 토지를 소유해야 건설 가능합니다.</li>
+                                    <li>⑤ 사업용 건물은 최소 100개 이상의 토지를 소유해야 건설 가능합니다.(3계위 이상)</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                
+            </Swiper>
+        </>
+    )
+}
+
+export default Auction
---
+++ src/app/pages/main-gate/frame/mypage/index.js
@@ -0,0 +1,595 @@
+import React, { useEffect, useState } from "react";
+import Style from "./style.module.css";
+import MyPageContent from "../../../my";
+import { useHistory, Link } from "react-router-dom";
+import Home from "../home/index";
+// 툴팁 라이브러리 설치했습니다
+import ReactTooltip from "react-tooltip";
+// 부트스트랩 라이브러리 설치했습니다.
+import NumberFormat from "react-number-format";
+// import styles from './OtherNumberFormat.module.scss'
+import { GiConfirmed } from "react-icons/gi";
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+
+const MyPage2 = ({
+  menubar,
+  footer,
+  points,
+  setPoints,
+  userUUID,
+  walletAddress,
+  setLoading,
+  setUserUUID,
+  setExchangeRate,
+  exchangeRate,
+  setCoin,
+  coin,
+  setEmail,
+  email,
+  setName,
+  name,
+  setPhone,
+  phone,
+  setEther,
+  ether,
+  setTempPoint,
+  setTempCoin,
+  tempPoint,
+  tempCoin,
+}) => {
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    setShow(true);
+  });
+  return (
+    <div className={Style["ground"]}>
+      {userUUID ? (
+        <MyPageContent
+          menubar={menubar}
+          footer={footer}
+          points={points}
+          setPoints={setPoints}
+          userUUID={userUUID}
+          walletAddress={walletAddress}
+          setLoading={setLoading}
+          setUserUUID={setUserUUID}
+          setExchangeRate={setExchangeRate}
+          exchangeRate={exchangeRate}
+          setCoin={setCoin}
+          coin={coin}
+          setEmail={setEmail}
+          email={email}
+          setName={setName}
+          name={name}
+          setPhone={setPhone}
+          phone={phone}
+          setEther={setEther}
+          ether={ether}
+          setTempPoint={setTempPoint}
+          setTempCoin={setTempCoin}
+          tempPoint={tempPoint}
+          tempCoin={tempCoin}
+        />
+      ) : undefined}
+    </div>
+  );
+};
+const characterName = ["dei", "ego", "el", "going"];
+function ExchangeCharacter(inputNum) {
+  if (inputNum >= 1 && inputNum < 10) {
+    return characterName[0];
+  }
+  if (inputNum >= 10 && inputNum < 100) {
+    return characterName[1];
+  }
+  if (inputNum >= 100 && inputNum < 1000) {
+    return characterName[2];
+  } else return characterName[3];
+}
+
+const ExchangeEwcCoin = ({ ether, coin, points }) => {
+  return (
+    <>
+      <span className={Style["exRugo"]} data-tip data-for="swewc1">
+        SW EWC
+      </span>
+      <ReactTooltip
+        id="swewc1"
+        place="left"
+        effect="solid"
+        backgroundColor="rgba(100,201,200,0.7)"
+      >
+        <span>SecurityWallet의 EWC개수 </span>
+        {/*언어변수값 들어갈곳*/}
+      </ReactTooltip>
+      <span className={Style["exRugoInput"]}>
+        <NumberFormat
+          value={ether}
+          thousandsGroupStyle="thousand"
+          type="text"
+          displayType="text"
+          thousandSeparator={true}
+          allowNegative={true}
+        />
+      </span>
+
+      <span className={Style["change-icon"]}>
+        <img
+          src={require("./images/icon3.png").default}
+          alt=""
+          width={"35px"}
+        ></img>
+      </span>
+
+      <span className={Style["exRugo"]} data-tip data-for="ewewc2">
+        EW EWC
+      </span>
+      <ReactTooltip
+        id="ewewc2"
+        place="left"
+        effect="solid"
+        backgroundColor="rgba(100,201,200,0.7)"
+      >
+        <span>Erugo World의 EWC(Point)개수 </span>
+        {/*언어변수값 들어갈곳*/}
+      </ReactTooltip>
+      <div type={Text} className={Style["exRugoInput"]}>
+        <NumberFormat
+          value={ether}
+          thousandsGroupStyle="thousand"
+          type="text"
+          displayType="text"
+          thousandSeparator={true}
+          allowNegative={true}
+        />
+      </div>
+      <button className={Style["exchange"]}>Exchange</button>
+      {/*언어변수값 들어갈곳*/}
+    </>
+  );
+};
+
+// 충전
+const ExchangeRugo = (props) => {
+  return (
+    <>
+      <span className={Style["exRugo"]} data-tip data-for="swewc3">
+      {props.SW}
+      </span>
+      {/* <ReactTooltip
+        id="swewc3"
+        place="left"
+        effect="solid"
+        backgroundColor="rgba(100,201,200,0.7)"
+      >
+        <span>Erugo World의 EWC(Point)개수 </span>
+      </ReactTooltip> */}
+      <input type={Text} className={Style["exRugoInput"]} placeholder={" 0 "} />
+
+      <span className={Style["change-icon"]}>
+        <img
+          src={require("./images/icon3.png").default}
+          alt=""
+          width={"35px"}
+        ></img>
+      </span>
+
+      <span className={Style["exRugo"]} data-tip data-for="ewewc4">
+        {props.EW}
+      </span>
+      {/* <ReactTooltip
+        id="ewewc4"
+        place="left"
+        effect="solid"
+        backgroundColor="rgba(100,201,200,0.7)"
+      >
+        <span>Erugo World의 RUGO(Point)개수 </span>
+      </ReactTooltip> */}
+      <input type={Text} className={Style["exRugoInput"]} placeholder={" 0 "} />
+      <button className={Style["exchange"]}>Exchange</button>
+    </>
+  );
+};
+
+// const Desktop = ({ children }) => {
+//   const isDesktop = useMediaQuery({ minWidth: 992 })
+//   return isDesktop ? children : null
+// }
+
+
+// page
+const MyPage = ({
+  menubar,
+  footer,
+  points,
+  setPoints,
+  userUUID,
+  walletAddress,
+  setLoading,
+  setUserUUID,
+  setExchangeRate,
+  exchangeRate,
+  setCoin,
+  coin,
+  setEmail,
+  email,
+  setName,
+  name,
+  setPhone,
+  phone,
+  setEther,
+  ether,
+  setTempPoint,
+  setTempCoin,
+  tempPoint,
+  tempCoin,
+}) => {
+  const history = useHistory();
+  const [accountCheck, setAccountCheck] = useState(new Set());
+  const accountCheckHandler = (amount, isChecked) => {
+    if (isChecked) {
+      accountCheck.add(amount);
+      setAccountCheck(accountCheck);
+    } else if (!isChecked && accountCheck.has(amount)) {
+      accountCheck.delete(amount);
+      setAccountCheck(accountCheck);
+    }
+  };
+  // console.log(`email : ${email}`);
+  // const todayVal = () => {
+  //   var randomVal = Math.ramdom
+  //   for(let i = 2500; i < 3000; i++) {
+  //     var randomVal2 = Math.floor(randomVal*10 + i)
+  //   }
+  //   return randomVal2
+  // }
+  //   const Input = ({ type='text', inputElement = null, style = {}, value, onChange, customEle }) => {
+  //     return (
+  //         <div style={{ width: '10%', height: '80px', display: 'flex', justifyContent: 'center', flexDirection: 'column', fontSize: '12px' }}>
+  //             <div style={{ width: '100%', height: '40px', display: 'flex', justifyContent: 'flex-start', alignItems: 'center' }}>
+  //                 <div style={{ width: '50%', height: '100%', display: 'flex', justifyContent: 'flex-start', alignItems: 'center' }}></div>
+  //                 <div style={{ width: '50%', height: '100%', display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>{customEle}</div>
+  //             </div>
+  //             <div style={{ width: '100%', height: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+  //                 {
+  //                     inputElement ? inputElement :
+  //                         <input
+  //                         type='text'
+  //                             className={Style['input']}
+  //                             style={{ width: '100%', height: '100%', padding: '10px', borderRadius: '4px', ...style }}
+  //                             value={value}
+  //                             onChange={
+  //                                 (e) => {
+  //                                     if (onChange && typeof onChange === 'function') {
+  //                                         onChange(e.target.value)
+  //                                     }
+  //                                 }
+  //                             }
+  //                         />
+  //                 }
+  //             </div>
+  //         </div>
+  //     )
+  // }
+const [txValue, setTxValue] = useState('');
+const txHandler = (e) => {
+let txValue= e.target.value
+setTxValue(txValue)
+}
+  return (
+    <>
+      <div style={{ overflowY: "auto" }} className={Style["mypage-bg"]}>
+        <div className={Style["character"]}>
+          <img
+            className={Style["character-img"]}
+            src={require(`./images/${ExchangeCharacter(coin)}.png`).default}
+            alt=""
+          />
+        </div>
+        <div className={Style["container"]}>
+          <div className={Style["mypageBox1"]}>
+            <div className={Style["pageBox1"]}>
+              <div className={Style["address"]}>
+                <table>
+                  <tr>
+                    <th>Email</th>
+                    {/*언어변수값 들어갈곳*/}
+                    <td>{email}</td>
+                  </tr>
+                  <tr>
+                    <th>Phone</th>
+                    {/*언어변수값 들어갈곳*/}
+                    <td>{phone}</td>
+                  </tr>
+                  <tr>
+                    <th>Name</th>
+                    {/*언어변수값 들어갈곳*/}
+                    <td>{name}</td>
+                  </tr>
+                  <tr>
+                  
+                    <th>Wallet</th>
+                    <td className={Style["wallet-width"]}>{walletAddress}</td>
+                    <CopyToClipboard text={walletAddress}>
+  <img src={require('./images/copy.png').default} alt="" data-tip
+                    data-for="copy" style={{cursor: "pointer", width: "25px"}}/>
+</CopyToClipboard>
+<ReactTooltip
+                    id="copy"
+                    place="left"
+                    effect="solid"
+                    backgroundColor="rgba(0,0,0,0.7)"
+                  >
+                    <span>copy</span>
+                    {/*언어변수값 들어갈곳*/}
+                  </ReactTooltip>
+                  </tr>
+                </table>
+              </div>
+              <div className={Style["line"]}></div>
+              <div className={Style["boxs"]}>
+                <div className={`${Style.box1} ${Style.boxLeft}`}>
+                  <div className={Style["info-title"]}>SecurityWallet 정보</div>
+                  {/*언어변수값 들어갈곳*/}
+                  <span
+                    className={Style["text-box"]}
+                    data-tip
+                    data-for="sw-eth"
+                  >
+                    SW ETH
+                  </span>
+                  <ReactTooltip
+                    id="sw-eth"
+                    place="top"
+                    effect="solid"
+                    backgroundColor="rgba(100,201,200,0.7)"
+                  >
+                    <span>SecurityWallet의 이더리움 개수</span>
+                    {/*언어변수값 들어갈곳*/}
+                  </ReactTooltip>
+                  <span className={Style["num-box"]}>
+                    <NumberFormat
+                      value={ether}
+                      thousandsGroupStyle="thousand"
+                      type="text"
+                      displayType="text"
+                      thousandSeparator={true}
+                      allowNegative={true}
+                    />
+                  </span>
+                </div>
+                <div className={`${Style.box1} ${Style.boxLeft}`}>
+                  <span
+                    className={Style["text-box"]}
+                    data-tip
+                    data-for="sw-ewc2"
+                  >
+                    SW EWC
+                  </span>
+                  <ReactTooltip
+                    id="sw-ewc2"
+                    place="top"
+                    effect="solid"
+                    backgroundColor="rgba(100,201,200,0.7)"
+                  >
+                    <span>SecurityWallet의 EWC개수</span>
+                    {/*언어변수값 들어갈곳*/}
+                  </ReactTooltip>
+                  <span className={Style["num-box"]}>
+                    <NumberFormat
+                      value={ether}
+                      thousandsGroupStyle="thousand"
+                      type="text"
+                      displayType="text"
+                      thousandSeparator={true}
+                      allowNegative={true}
+                    />
+                  </span>
+                </div>
+                <div className={`${Style.box1} ${Style.boxRight}`}>
+                  <div className={Style["info-title"]}>ErugoWorld 정보</div>
+                  {/*언어변수값 들어갈곳*/}
+                  <span
+                    className={Style["text-box"]}
+                    data-tip
+                    data-for="ew-ewc3"
+                  >
+                    EW EWC
+                  </span>
+                  <ReactTooltip
+                    id="ew-ewc3"
+                    place="top"
+                    effect="solid"
+                    backgroundColor="rgba(100,201,200,0.7)"
+                  >
+                    <span>Erugo World의 EWC(Point)개수</span>
+                    {/*언어변수값 들어갈곳*/}
+                  </ReactTooltip>
+                  <span className={Style["num-box"]}>
+                    <NumberFormat
+                      value={coin}
+                      thousandsGroupStyle="thousand"
+                      type="text"
+                      displayType="text"
+                      thousandSeparator={true}
+                      allowNegative={true}
+                    />
+                  </span>
+                </div>
+                <div className={`${Style.box1} ${Style.boxRight}`}>
+                  <span
+                    className={Style["text-box"]}
+                    data-tip
+                    data-for="ew-rugo"
+                  >
+                    EW Rugo
+                  </span>
+                  <ReactTooltip
+                    id="ew-rugo"
+                    place="top"
+                    effect="solid"
+                    backgroundColor="rgba(100,201,200,0.7)"
+                  >
+                    <span>Erugo World의 Rugo 개수</span>
+                    {/*언어변수값 들어갈곳*/}
+                  </ReactTooltip>
+                  <span className={Style["num-box"]}>
+                    <NumberFormat
+                      value={points}
+                      thousandsGroupStyle="thousand"
+                      type="text"
+                      displayType="text"
+                      thousandSeparator={true}
+                      allowNegative={true}
+                    />
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className={Style["mypageBox2"]}>
+            <div className={Style["pageBox2"]}>
+              <div className={Style["exchange-xt"]}>
+                <div className={Style["title"]}>거래소에서 충전하기</div>
+                {/*언어변수값 들어갈곳*/}
+                <span
+                  className={Style["input-content"]}
+                  data-tip
+                  data-for="xtcom"
+                >
+                  XT.COM
+                </span>            
+                <div
+                    className={Style["help-btn"]}
+                    onClick={() => {
+                      history.push(
+                        "https://blog.naver.com/erugocoin_official/222663552969"
+                      );
+                    }}
+                  ></div>
+                <ReactTooltip
+                  id="xtcom"
+                  place="left"
+                  effect="solid"
+                  backgroundColor="rgba(100,201,200,0.5)"
+                >
+                  <span>거래소 XT.COM</span>
+                  {/*언어변수값 들어갈곳*/}
+                </ReactTooltip>
+                
+                <input
+                  type="text"
+                  className={Style["input-box"]}
+                  placeholder={" TX(transaction hash)값"}
+                  value={txValue}
+                  onChange={txHandler}
+                />
+                {
+                  txValue && txValue !== "" ? <img src={require('./images/checked.png').default} alt="" style={{width: "30px"}}/> : 
+                  <img src={require('./images/unchecked.png').default} alt="" style={{width: "30px"}}/>
+                }
+               
+                {/* <div className={Style["xtScale"]}>
+                  <button
+                    data-tip
+                    data-for="info"
+                    id="xtComInfo"
+                    className={Style["xtComInfo"]}
+                    onClick={() => {
+                      history.push(
+                        "https://blog.naver.com/erugocoin_official/222663552969"
+                      );
+                    }}
+                  >
+                    <ReactTooltip id="info" place="left" effect="solid" backgroundColor="rgba(100,201,200,0.7)">
+                      <span>자세한 설명은 클릭</span>
+           
+                    </ReactTooltip>
+                  </button>
+                </div> */}
+                <div style={{height: "5vh"}}/>
+                <div style={{ position: "relative" }}>
+                  <span
+                    className={Style["title"]}
+                    data-tip
+                    data-for="sw-change"
+                  >
+                    SecurityWallet 충전하기
+                  </span>
+                  <ExchangeRugo SW={'SW EWC'} EW={'EW EWC'}/>
+
+
+                  <ReactTooltip
+                    id="sw-change"
+                    place="top"
+                    effect="solid"
+                    backgroundColor="rgba(70,201,200,0.7)"
+                  >
+                    <span>
+                      SecurityWallet에 있는 EWC를 ErugoWorld EWC로 충전시키기
+                    </span>
+                    {/*언어변수값 들어갈곳*/}
+                  </ReactTooltip>
+                  <div
+                    className={Style["help-btn"]}
+                    onClick={() => {
+                      history.push(
+                        "https://blog.naver.com/erugocoin_official/222663552969"
+                      );
+                    }}
+                  ></div>
+                </div>
+              </div>
+              
+              <div className={Style["bg-down"]}>
+                <div className={Style["box-bottom"]}>
+                  <span
+                    className={Style["title"]}
+                    data-tip
+                    data-for="rugo-change"
+                  >
+                    RUGO로 변환하기
+                  </span>
+                  {/*언어변수값 들어갈곳*/}
+                  <ReactTooltip
+                    id="rugo-change"
+                    place="top"
+                    effect="solid"
+                    backgroundColor="rgba(70,201,200,0.7)"
+                  >
+                    <span>EWC를 RUGO로 변환시켜드립니다.</span>
+                    {/*언어변수값 들어갈곳*/}
+                  </ReactTooltip>
+                  <div
+                    className={Style["help-btn"]}
+                    onClick={() => {
+                      history.push(
+                        "https://blog.naver.com/erugocoin_official/222663552969"
+                      );
+                    }}
+                  ></div>
+                  <ExchangeRugo SW={'EW EWC'} EW={'EW Rugo'}/>
+                  <p>CURRENT EXCHANGE RATE : 1 Erugo Coin = 3000 Point </p>
+                </div>
+              </div>
+              <button
+                className={Style["logout-btn"]}
+                onClick={() => {
+                  setUserUUID(undefined);
+                  sessionStorage.removeItem("userUUID");
+                  sessionStorage.removeItem("walletAddress");
+                  history.push("/main-entrance");
+                }}
+              >
+                로그아웃
+              </button>
+              {/*언어변수값 들어갈곳*/}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default MyPage;
